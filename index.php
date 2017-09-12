346be6685347e6b735e495ef8e55c71b0b85e883
Waleed Ahmad <waleedgplus@gmail.com>
2017-09-12 05:02:44 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Reddit Scrape</title>
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="src/style.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-default">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="/">Reddit Scrape</a>
+				</div>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container">
+		<div id="app" data-nav-after="<?=isset($_GET['after']) ? $_GET['after'] : 'NULL' ?>"></div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="src/app.js"></script>
+</body>
+</html>
---
+++ src/app.js
@@ -0,0 +1,122 @@
+let $app = $('#app'),
+    nav_after_id = $($app).attr('data-nav-after'),
+    req_params = (nav_after_id !== 'NULL') ? '?after='+nav_after_id : '',
+    count = -1;
+
+$.getJSON('https://www.reddit.com/r/Models.json'+req_params, function(response) {
+    $.each(response.data.children, function(i, item) {
+        if (item.kind === 't3') {
+            if (isImage(item.data.url)) {
+                renderImage(item.data.title, item.data.url, item.data.id);
+            } else {
+                if (item.data.domain === 'imgur.com') {
+                    renderImageOrAlbum(item);
+                }
+            }
+        }
+    });
+    renderNavigation(response.data.after);
+});
+
+function renameListener(e){
+    let id = $(this).attr('data-id');
+    $(".post-name[data-id='" + id +"']").val(this.value);
+}
+
+function renderNavigation(after_id){
+    let $nav = `<div class="navigation">
+                <nav>
+                    <ul class="pager">
+                        <li class="next"><a href="?after=${after_id}">More <span aria-hidden="true">&rarr;</span></a></li>
+                    </ul>
+                </nav>
+            </div>`;
+    $($app).after($nav);
+}
+
+function renderImage(title,url,id) {
+    $post = `<div class="post">
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 image">
+				    <img class="image-uri" src="${url}">
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 title">
+				  	<div class="input-group">
+				  	    <span class="input-group-btn">
+					        <button class="btn btn-default save-nsfw" data-type="NSFW" type="button"> <span class="glyphicon glyphicon-save" aria-hidden="true"></span> NSFW</button>
+					    </span>
+					    <input class="form-control post-name" value="${title}" data-id="${id}" placeholder="Name..">
+					    <span class="input-group-btn">
+					        <button class="btn btn-default save-safe" data-type="SAFE" type="button"> <span class="glyphicon glyphicon-save" aria-hidden="true"></span> SAFE </button>
+					    </span>
+					   
+				    </div>
+				</div>
+			 </div>`;
+    $.when($($app).append($post)).then(function(){
+        ++count;
+        $('.post-name').eq(count).on('keyup', renameListener);
+        $('.save-nsfw').eq(count).on('click', saveImage);
+        $('.save-safe').eq(count).on('click', saveImage);
+    });
+}
+
+function saveImage(e){
+    let type = $(this).attr('data-type'),
+        $post = $(this).parents('.post'),
+        name = $.trim($($post).find('.post-name').val()),
+        url = $($post).find('.image-uri').attr('src');
+
+    $.ajax({
+        type : 'POST',
+        url : '/save.php',
+        data : {
+            type : type,
+            name : name,
+            url : url
+        },
+        success : function(res){
+            if(JSON.parse(res) === true){
+                $($post).remove();
+            }else{
+                $(this).on('click', saveImage);
+                alert('Unable to save image');
+            }
+        }.bind(this)
+    })
+}
+
+function renderImageOrAlbum(item) {
+
+    let albumID = stripAlumbId(item.data.url),
+        type = item.data.url.indexOf('/a/') !== -1 ? 'album' : 'image',
+        albumAPI = "https://api.imgur.com/3/"+type+"/" + albumID + "/images";
+
+    $.ajax({
+        url: albumAPI,
+        headers: {
+            'Authorization': 'Client-ID fc6952f445a03f3'
+        },
+        type: 'GET',
+        dataType: 'json',
+        success: function(response) {
+        	if(response.data.length){
+        		$.each(response.data, function(i, res){
+	        		renderImage(item.data.title, res.link, item.data.id);
+	        	});
+        	}else{
+        		renderImage(item.data.title, response.data.link, item.data.id)
+        	}
+        },
+        error: function(err) {
+        	console.log(err);
+        }
+    });
+}
+
+function isImage(url) {
+    return (url.match(/\.(jpeg|jpg|gif|png)$/) !== null);
+}
+
+function stripAlumbId(url) {
+    return /[^/]*$/.exec(url)[0];
+}
